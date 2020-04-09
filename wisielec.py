a7e1ae46b1b2c233f553df88e9bd4d0fcde76712
lorzel <lukaszorzell@gmail.com>
2020-04-09 22:30:49 +0200

    Update wisielec.py
    Added dict_to_string which convert dictionary to string sentence

--- wisielec.py
+++ wisielec.py
@@ -1,9 +1,8 @@
-# words = {'reguły są poto, aby je łamać'}dsadadasd
-
 sentence_main = 'reguły są po to, aby je łamać'
 letters_list = []
 list = []
 list2 = []
+dict_list = []
 alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'o', 'ó', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z', 'ż', 'ź']
 
@@ -32,7 +31,7 @@ def generate_stars(words):
                 i += 1
                 list.append(letter)
             star_sentence = ''.join(list)
-    print(star_sentence)
+    #print(star_sentence)
 
 
 def update_stars(sentence, char):
@@ -58,15 +57,21 @@ def update_stars(sentence, char):
                 dictionary2 = {i: letter}
                 dictionary.update(dictionary2)
                 i += 1
+    dict_to_string(dictionary)
 
-    print(dictionary)
+
+def dict_to_string(dict):
+    for x in dict.values():
+        dict_list.append(x)
+    print("".join(dict_list))
+    dict_list.clear()
+
 
 
 sentence_to_letters(sentence_main)
 generate_stars(sentence_main)
-print(dictionary)
+
 update_stars(sentence_main, '')
-#cos
 user_input = input('Litera: ')
 update_stars(sentence_main, user_input)
 user_input2 = input('Litera: ')
